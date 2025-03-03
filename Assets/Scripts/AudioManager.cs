8a1c691e2f5bb0293d2597d48ce085917d0b56d8
i3amero <lsyskc15@gmail.com>
2025-03-03 23:14:52 +0900

    낮 건물진입 BGM 이어지는 기능, 리소스 추가

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // 씬이 변경되어도 파괴되지 않음
+        }
+        else
+        {
+            Destroy(gameObject); // 기존 인스턴스가 있으면 새로운 것은 삭제
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneWithBGMStop(string sceneName)
+    {
+        // AudioManager가 존재하면 제거
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+        if (audioManager != null)
+        {
+            Destroy(audioManager.gameObject);
+        }
+
+        // 씬 전환
+        SceneManager.LoadScene(sceneName);
+    }
+}
