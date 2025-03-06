8413a22ef238d18f36df0a93cba31ea371553082
i3amero <lsyskc15@gmail.com>
2025-03-07 03:06:06 +0900

    여명 정상화

--- Assets/Scripts/ScenarioController.cs
+++ Assets/Scripts/ScenarioController.cs
@@ -25,6 +25,7 @@ public class ScenarioController : MonoBehaviour
 
     private void Start()
     {
+        Debug.Log($"ScenarioController.Instance: {ScenarioController.Instance}");
         LoadUnlockedScenarios();
     }
 
